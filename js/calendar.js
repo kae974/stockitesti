f59959ebf21f2df463aac139a6bdfe017d93e6a5
kae974 <kae.knou@outlook.fr>
2026-07-21 19:28:17 +0200

    Refactor calendar.js with French variable names

--- js/calendar.js
+++ js/calendar.js
@@ -1,92 +1,287 @@
-let calendar;
+let calendrier;
 
 
-async function chargerDonnees() {
+const DUREE_MINIMUM = 30;
 
-    const response = await fetch("data/reservations.json");
+const PRIX = 60;
 
-    const data = await response.json();
 
-    return data.boxes;
+
+async function chargerBoxes(){
+
+const reponse = await fetch("data/reservations.json");
+
+const donnees = await reponse.json();
+
+return donnees.boxes;
+
+}
+
+
+
+
+
+function ajouterJours(date, nombre){
+
+
+let nouvelleDate = new Date(date);
+
+
+nouvelleDate.setDate(
+nouvelleDate.getDate()+nombre
+);
+
+
+return nouvelleDate.toISOString().split("T")[0];
+
 
 }
 
 
 
-async function afficherCalendrier(boxId) {
 
 
-    const boxes = await chargerDonnees();
+function verifierPeriode(debut, fin, reservations){
 
 
-    const box = boxes.find(
-        element => element.id === boxId
-    );
+let debutClient = new Date(debut);
 
-
-    let evenements = [];
-
-
-    box.reservations.forEach(reservation => {
-
-
-        evenements.push({
-
-            title: "Occupé",
-
-            start: reservation.debut,
-
-            end: reservation.fin,
-
-            display: "background",
-
-            backgroundColor: "#ff4d4d"
-
-        });
-
-
-    });
+let finClient = new Date(fin);
 
 
 
-    if(calendar){
-
-        calendar.destroy();
-
-    }
+return !reservations.some(res=>{
 
 
+let debutReserve = new Date(res.debut);
 
-    calendar = new FullCalendar.Calendar(
-
-        document.getElementById("calendar"),
-
-
-        {
-
-            initialView: "dayGridMonth",
-
-            locale: "fr",
+let finReserve = new Date(res.fin);
 
 
-            selectable: true,
+
+return (
+debutClient <= finReserve &&
+finClient >= debutReserve
+);
 
 
-            events: evenements,
-
-
-            height: "auto"
-
-
-        }
-
-    );
-
-
-    calendar.render();
+});
 
 
 }
+
+
+
+
+
+
+
+async function afficherDisponibilites(){
+
+
+let boxes = await chargerBoxes();
+
+
+let zone = document.getElementById(
+"disponibilites"
+);
+
+
+zone.innerHTML="";
+
+
+
+boxes.forEach(box=>{
+
+
+let occupe = verifierPeriode(
+new Date().toISOString().split("T")[0],
+new Date().toISOString().split("T")[0],
+box.reservations
+);
+
+
+
+let div=document.createElement("div");
+
+
+div.className="box";
+
+
+
+if(occupe){
+
+div.innerHTML =
+"🔴 "+box.nom+" Occupé";
+
+}
+
+else{
+
+div.innerHTML =
+"🟢 "+box.nom+" Disponible";
+
+}
+
+
+
+zone.appendChild(div);
+
+
+
+});
+
+
+}
+
+
+
+
+
+
+
+async function afficherCalendrier(idBox){
+
+
+let boxes = await chargerBoxes();
+
+
+
+let box = boxes.find(
+b=>b.id===idBox
+);
+
+
+
+let evenements=[];
+
+
+
+box.reservations.forEach(res=>{
+
+
+evenements.push({
+
+title:"Occupé",
+
+start:res.debut,
+
+end:res.fin,
+
+display:"background",
+
+backgroundColor:"#ff4444"
+
+
+});
+
+
+});
+
+
+
+
+
+if(calendrier){
+
+calendrier.destroy();
+
+}
+
+
+
+
+
+calendrier = new FullCalendar.Calendar(
+
+document.getElementById("calendar"),
+
+
+{
+
+initialView:"dayGridMonth",
+
+locale:"fr",
+
+
+events:evenements,
+
+
+dateClick:function(info){
+
+
+
+let debut = info.dateStr;
+
+
+let fin = ajouterJours(
+debut,
+DUREE_MINIMUM
+);
+
+
+
+document.getElementById("dateSelection").innerHTML =
+
+"Début : "+debut;
+
+
+
+document.getElementById("dateFin").innerHTML =
+
+"Fin minimum : "+fin;
+
+
+
+if(
+verifierPeriode(
+debut,
+fin,
+box.reservations
+)
+
+){
+
+
+document.getElementById("resultatReservation").innerHTML =
+
+"🟢 Disponible<br><br>"+
+"Location minimum 30 jours<br>"+
+"Prix : "+PRIX+" €";
+
+
+}
+
+else{
+
+
+document.getElementById("resultatReservation").innerHTML =
+
+"🔴 Cette période est indisponible";
+
+
+}
+
+
+
+}
+
+
+}
+
+
+);
+
+
+
+calendrier.render();
+
+
+
+}
+
+
+
 
 
 
@@ -94,17 +289,18 @@ async function afficherCalendrier(boxId) {
 document
 .getElementById("choixBox")
 .addEventListener(
-
 "change",
-
 function(){
 
-    afficherCalendrier(this.value);
+afficherCalendrier(this.value);
 
 }
 
 );
 
+
+
+afficherDisponibilites();
 
 
 afficherCalendrier("box1");
