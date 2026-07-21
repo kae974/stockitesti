ccff6201e7262fbb21d4214706ad1efdf032ff1c
kae974 <kae.knou@outlook.fr>
2026-07-21 15:01:31 +0200

    Update reservation.js

--- reservation.js
+++ reservation.js
@@ -3,3 +3,34 @@ function choisirBox(box){
 document.getElementById("boxChoisi").value = box;
 
 }
+function choisirBox(nomBox) {
+    document.getElementById("boxChoisi").value = nomBox;
+}
+
+fetch("box.json")
+  .then(response => response.json())
+  .then(data => {
+
+    const conteneur = document.querySelector(".boxes");
+    conteneur.innerHTML = "";
+
+    data.boxes.forEach(box => {
+
+      const bouton = document.createElement("button");
+
+      bouton.disabled = !box.disponible;
+
+      bouton.innerHTML =
+        `${box.nom}<br>${
+          box.disponible ? "🟢 Disponible" : "🔴 Occupé"
+        }`;
+
+      if (box.disponible) {
+        bouton.onclick = () => choisirBox(box.nom);
+      }
+
+      conteneur.appendChild(bouton);
+
+    });
+
+  });
